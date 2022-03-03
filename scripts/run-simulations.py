01fe8fc3a4bd890d455c3fd0c5620784a42c7417
Lev Stambler <levstamb@gmail.com>
2022-03-03 14:27:08 -0500

    scripts

--- scripts/run-simulations.py
+++ scripts/run-simulations.py
@@ -1,6 +1,12 @@
+import os
 from pandas import cut
 import shared
 # Assume that the codes are created
+
+runMatlabCmd = """
+matlab -nodisplay -nojvm -nosplash -nodesktop -sd "/home/lev/code/research/pfm/matlab/" -r "try, run('/home/lev/code/research/pfm/matlab/get_block_error.m'), catch, exit(1), end, exit(0);"
+echo "matlab exit code: $?"
+"""
 
 error_ps = [0.005, 0.01, 0.02, 0.03, 0.04]
 ks = range(5, 8)
@@ -12,5 +18,8 @@ for i in ks:
     for p in error_ps:
         paramsf = open('../matlab/tmp/params.json', "w")
         paramsf.write(
-            "{" + f'"p": {p}, "n_trials": {Ntrials}, "cutoff": {cutoff}, "file_out": "../matlab/out/{i * regularity[1]}-{i * regularity[0]}-graph-{p}-{Ntrials}-{cutoff}.json"' + "}"
+            "{" +
+            f'"p": {p}, "n_trials": {Ntrials}, "cutoff": {cutoff}, "file_out": "../matlab/out/{i * regularity[1]}-{i * regularity[0]}-graph-{p}-{Ntrials}-{cutoff}.json"' + "}"
         )
+        os.system(runMatlabCmd)
+				# print(runMatlabCmd
